13d4551bafa6b07ec6736fda8926f2c4b13bf2aa
BrendaOwinyo <breapondi@gmail.com>
2025-07-25 12:15:41 +0300

    different urls

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
 app= Flask(__name__)
-@app.route('/')
+@app.route('/add/<int:number1>/<int:number2>')
 
-def index():
-    return "<h1>Hello World</h1>"
+def add(number1, number2):
+    return f" {number1} + {number2} = {number1+number2}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
